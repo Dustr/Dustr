0c4e78345220b61e18a648aa338aa754244d1241
Hamza Ghani <hamza.ghani@utexas.edu>
2017-07-16 17:36:03 -0500

    loads from camera roll and can cycle through photos

--- DustrApp/DustrApp/ViewController.swift
+++ DustrApp/DustrApp/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+   var imgArray = [UIImage]()
+   var index = 0
+    
     
     
     @IBOutlet weak var currentImage: UIImageView!
+   
     
     @IBAction func addPhoto(_ sender: Any) {
         let image = UIImagePickerController()
@@ -27,6 +32,42 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             //after it is complete
         }
 
+    }
+    
+    
+    func loadPhotos() {
+        let imagesManager = PHImageManager.default()
+        
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.deliveryMode = .opportunistic    //balance between image quality and speed
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        
+        if let fetchResult: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions) {
+            // if there images
+            if fetchResult.count > 0 {
+                for i in 0..<fetchResult.count {
+                    
+                    imagesManager.requestImage(for: fetchResult.object(at: i) as PHAsset, targetSize: CGSize(width: 200, height: 200), contentMode: .aspectFill, options: requestOptions, resultHandler: {
+                        image, error in
+                        
+                        self.imgArray.append(image!)
+                    })
+                }
+            }
+            
+        }
+
+        
+        
+    }
+    
+    
+    @IBAction func nextPhoto(_ sender: Any) {
+        currentImage.image = imgArray[index]
+        index += 1
+        
         
     }
     
@@ -45,7 +86,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadPhotos()
+        
+      
         // Do any additional setup after loading the view.
     }
 

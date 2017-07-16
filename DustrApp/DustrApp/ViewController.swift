001c04d39fb324fe221d732a185a38d7431ec0ac
Hamza Ghani <hamza.ghani@utexas.edu>
2017-07-16 13:51:58 -0500

    Basic photo importing from iphone Library

--- DustrApp/DustrApp/ViewController.swift
+++ DustrApp/DustrApp/ViewController.swift
@@ -2,24 +2,67 @@
 //  ViewController.swift
 //  DustrApp
 //
-//  Created by Hamza Ghani on 7/6/17.
+//  Created by Hamza Ghani on 7/9/17.
 //  Copyright © 2017 Dustr. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    
+    
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    @IBAction func addPhoto(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        image.allowsEditing = false
+        
+        self.present(image, animated: true) {
+            
+            //after it is complete
+        }
+
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            currentImage.image = image
+        }
+            
+        else {
+            //error message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-

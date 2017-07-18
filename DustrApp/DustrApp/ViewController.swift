888f5758ecd28fca2769bbacddd5f3f52d102fb4
Frank Alvarado <franklink1829@gmail.com>
2017-07-17 22:39:45 -0500

    Swiping works, click to enlarge

--- DustrApp/DustrApp/ViewController.swift
+++ DustrApp/DustrApp/ViewController.swift
@@ -7,29 +7,18 @@
 //
 
 import UIKit
+import Photos
 import Koloda
-
-private var numberOfCards: Int = 5
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var kolodaView: KolodaView!
     
-    
-    
-    fileprivate var dataSource: [UIImage] = {
-        var array: [UIImage] = []
-        for index in 0..<numberOfCards {
-            array.append(UIImage(named: "Dustr_\(index + 1)")!)
-        }
-        
-        return array
-    }()
-    
+    fileprivate var dataSource = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadPhotos()
         
         kolodaView.dataSource = self
         kolodaView.delegate = self
@@ -55,21 +44,55 @@ class ViewController: UIViewController {
         kolodaView?.revertAction()
     }
     
-    
+    func loadPhotos() {
+        let imagesManager = PHImageManager.default()
+        
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.deliveryMode = .opportunistic
+        requestOptions.isSynchronous = true
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        
+        if let fetchResult: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions) {
+            // if there images
+            if fetchResult.count > 0 {
+                for i in 0..<fetchResult.count {
+                    imagesManager.requestImage(for: fetchResult.object(at: i) as PHAsset, targetSize: CGSize(width:710 , height:800), contentMode: .aspectFit, options: requestOptions, resultHandler: {
+                        image, error in
+                        self.dataSource.append(image!)
+                    })
+                }
+            }
+        }
+    }
 }
 
 extension ViewController: KolodaViewDelegate {
     
     func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
-        let position = kolodaView.currentCardIndex
-        for i in 1...4 {
-            dataSource.append(UIImage(named: "Dustr_\(i)")!)
-        }
-        kolodaView.insertCardAtIndexRange(position..<position + 4, animated: true)
+        kolodaView.resetCurrentCardIndex()
     }
     
     func koloda(_ koloda: KolodaView, didSelectCardAt index: Int) {
-        UIApplication.shared.open(URL(string: "https://yalantis.com/")!, options: [:], completionHandler: nil)
+        //UIApplication.shared.open(URL(string: "https://yalantis.com/")!, options: [:], completionHandler: nil)
+        let fullscreenPhoto = UIImageView(image: dataSource[Int(index)])
+        let newImageView = UIImageView(image: fullscreenPhoto.image)
+        newImageView.frame = UIScreen.main.bounds
+        newImageView.backgroundColor = .black
+        newImageView.contentMode = .scaleAspectFit
+        newImageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
+        newImageView.addGestureRecognizer(tap)
+        self.view.addSubview(newImageView)
+        self.navigationController?.isNavigationBarHidden = true
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    func dismissFullscreenImage(_ sender: UITapGestureRecognizer) {
+        self.navigationController?.isNavigationBarHidden = false
+        self.tabBarController?.tabBar.isHidden = false
+        sender.view?.removeFromSuperview()
     }
     
 }
